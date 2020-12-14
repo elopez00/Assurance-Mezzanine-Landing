912552c5d301f5ebfa6b5253c28885b24dd5eac7
Eddie Lopez <ejlopez00@gmail.com>
2020-12-14 12:28:39 -0500

    updated home-screen

--- src/components/home/Banner/Banner.js
+++ src/components/home/Banner/Banner.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import './Banner.css'
+import Button from '../../layout/Button'
 
 export default function Banner(props) {
     return (
         <div id="aml-home-banner">
             <div id="aml-home-screen">
                 <div>
-                    <span>ASSURANCE MEZZANINE</span>
-                    <p> Assurance Mezzanine Fund is a private investment firm providing $3 to $20 
-                        million of customized growth solutions to profitable, lower-middle-market 
-                        companies.
+                    <span>Assurance Mezzanine Fund</span>
+                    <div id="aml-home-screen-divider"/>
+                    <p> Assurance Mezzanine Fund is a private investment firm providing 
+                        $3 to $20 million of customized growth solutions to profitable, 
+                        lower-middle-market companies.
                     </p>
+                    <Button variant="trans-white" style={{ marginRight: 20}}>Portfolio</Button>
+                    <Button variant="trans-white">Contact</Button>
                 </div>
             </div>
         </div>
--- src/components/static/Header/Header.js
+++ src/components/static/Header/Header.js
@@ -1,32 +1,21 @@
 import React from 'react'
 import './Header.css'
+import logo from '../../../assets/trans-logo.png'
+import Button from '../../layout/Button'
 
 export default function Header(props) {
     return (
         <div className="aml-header">
             <div id="aml-header-navbar">
-                <div id="aml-header-nav">
-                    <div id="arrow"/>
-                    <span>PORTFOLIO</span>
+                <div id="aml-header-navs">
+                    <img src={logo} height="75"/>
+                    <span>Portfolio</span>
+                    <span>Investment Criteria</span>
+                    <span>Team</span>
+                    <span>News</span>
+                    <span>Contact</span>
                 </div>
-                <div id="aml-header-nav">
-                    <div id="arrow"/>
-                    <span>INVESTMENT CRITERIA</span>
-                </div>
-                <div id="aml-header-logo">
-                </div>
-                <div id="aml-header-nav">
-                    <div id="arrow"/>
-                    <span>TEAM</span>
-                </div>
-                <div id="aml-header-nav">
-                    <div id="arrow"/>
-                    <span>NEWS</span>
-                </div>
-                <div id="aml-header-nav">
-                    <div id="arrow"/>
-                    <span>CONTACT</span>
-                </div>
+                <Button variant="trans-white">Login</Button>
             </div>
         </div>
     )
